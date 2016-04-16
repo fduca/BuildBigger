83a6fa1843c27e7d0106df918266665e0354c436
cudaf <cudaf@admins-MacBook-Pro-6.local>
2016-04-16 19:53:17 +0100

    fix : fix remote configuration for the app engine

--- app/src/main/java/com/udacity/gradle/builditbigger/async/EndpointGCMAsyncTask.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/async/EndpointGCMAsyncTask.java
@@ -33,12 +33,12 @@ public class EndpointGCMAsyncTask  extends AsyncTask<Pair<Context, String>, Void
     @Override
     protected String doInBackground(Pair<Context, String>... params) {
         if(mJokeApi == null) {  // Only do this once
-            JokeApi.Builder builder = new JokeApi().Builder(AndroidHttp.newCompatibleTransport(),
+            JokeApi.Builder builder = new JokeApi.Builder(AndroidHttp.newCompatibleTransport(),
                     new AndroidJsonFactory(), null)
                     // options for running against local devappserver
                     // - 10.0.2.2 is localhost's IP address in Android emulator
                     // - turn off compression when running against local devappserver
-                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                    .setRootUrl("http://10.0.2.2/_ah/api/")
                     .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
                         @Override
                         public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
